80bf91b71f0be036df6b9f6090a780fda6538708
Cristian Martín <coal182@gmail.com>
2023-11-23 12:50:08 +0100

    reduce exercises

--- src/index.ts
+++ src/index.ts
@@ -1,31 +1,16 @@
-import {promiseMethods} from './promises';
-import {arrayMethods} from './arrays';
-import {objectMethods} from './objects';
-import {enumMethods} from './enums';
-import {lodashMethods} from './lodash';
-import {giveMeTheFirstValueAvailable} from './clean-code';
-import {spread} from './spread';
-import {pipeExample} from './pipe';
-import {executeFizzBuzz} from './fizzbuzz/';
-import {subject} from './rxjs/subject';
-import {getPokemonsIds} from '../test/stub';
-import {StudentSignUpper} from './value-objects/application/sign-up/student-sign-upper';
-import {StudentPassword} from './value-objects/domain/student-password';
-import {StudentName} from './value-objects/domain/student-name';
-import {StudentId} from './value-objects/domain/student-id';
-import {v4 as uuid} from 'uuid';
-import {typesFunction} from './types-function';
+import { reduces } from './reduces';
 
 //arrayMethods();
 //objectMethods();
 //enumMethods();
-lodashMethods();
+//lodashMethods();
 //promiseMethods();
 //giveMeTheFirstValueAvailable();
 //spread();
 //executeFizzBuzz(100);
 //pipeExample();
 //subject();
+reduces()
 /*
 const pokemons = getPokemonsIds(9)
     .then((pokemons) => {
